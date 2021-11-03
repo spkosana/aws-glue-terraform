707dc1e8f316d974e1ff9508c82610ce786ede5e
spkosana <spkosana.82@gmail.com>
2021-11-02 23:22:22 -0400

    adding terraform code

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "us-east-2"
+}
+
+variable "profile" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "terraform"
+}
